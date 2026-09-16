9393e6126977cf567f563ca52ab0ad8397aad2cc
Udin-Developer <rezarandikap@gmail.com>
2026-09-16 10:21:57 +0800

    feat: Penambahan Properti lokasi, fungsi find, dan destructing

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
-import { ringkasKarya } from './utils.js';
+import { ringkasKarya, cariBerdasarkanId } from './utils.js';
 
 const katalogMusik = [
     { id: 'K001', judul: 'Suar', musisi: 'Moeremans', genre: 'Alternative Rock, indie rock', durasi: 5.50 },
     { id: 'K002', judul: 'Vas Bunga', musisi: 'Lomba Sihir', genre: 'Alternative Pop', durasi: 3.22 },
     { id: 'K003', judul: 'Penghujung Cerita', musisi: 'Murphy Radio', genre: 'Midwest', durasi: 4.45 }
 ];
+
+const musikTarakan = katalogMusik.filter(item => item.lokasi === 'Tarakan');
+console.log("KARYA LOKAL TARAKAN");
+console.table(musikTarakan);
 
 const musikAkustik = katalogMusik.filter(item => item.genre === 'Midwest');
 
@@ -25,3 +29,14 @@ try {
 } catch (error) {
     console.error("Terjadi kesalahan sistem:", error.message);
 }
+
+const hasilPencarian = cariBerdasarkanId(katalogMusik, 'K002');
+console.log("PENCARIAN ID K002");
+console.log(hasilPencarian);
+
+const ringkasanLengkap = katalogMusik.map(({ judul, musisi, lokasi }) => {
+    return `Lagu "${judul}" dibawakan oleh ${musisi} dari wilayah ${lokasi}.`;
+});
+
+console.log("RINGKASAN DATA KATALOG MUSIK");
+ringkasanLengkap.forEach(teks => console.log(teks));
--- js/utils.js
+++ js/utils.js
@@ -1,12 +1,17 @@
 export function ringkasKarya(data) {
-    
+
     if (!Array.isArray(data)) {
         throw new TypeError('Data harus berupa array');
     }
-    
+
     return {
         totalKarya: data.length,
         totalDurasi: data.reduce((sum, item) => sum + item.durasi, 0),
         jumlahMidwest: data.filter(item => item.genre === 'Midwest').length
     };
+}
+
+export function cariBerdasarkanId(data, idDicari) {
+    if (!Array.isArray(data)) throw new TypeError('Data harus berupa array');
+    return data.find(item => item.id === idDicari);
 }
